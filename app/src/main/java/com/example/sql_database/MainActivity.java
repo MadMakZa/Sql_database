79169e021c5dd5da415e09d986baa37be4473606
MadMakZa <immakza@gmail.com>
2020-12-05 15:55:05 +0300

    method init

--- app/src/main/java/com/example/sql_database/MainActivity.java
+++ app/src/main/java/com/example/sql_database/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sql_database;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +10,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.sql_database.adapter.MainAdapter;
 import com.example.sql_database.db.MyDbManager;
 
 public class MainActivity extends AppCompatActivity {
     private MyDbManager myDbManager;
     private EditText edTitle, edDisc;
+    private RecyclerView rcView;
+    private MainAdapter mainAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    //метод для инициализации переменных
+    private void init(){
         myDbManager = new MyDbManager(this);
         edTitle = findViewById(R.id.edTitle);
         edDisc = findViewById(R.id.edDisc);
+        rcView = findViewById(R.id.rcView);
+        mainAdapter = new MainAdapter(this);
+        //расположение элементов по вертикали
+        rcView.setLayoutManager(new LinearLayoutManager(this));
+        rcView.setAdapter(mainAdapter);
+
+
     }
 
     @Override
