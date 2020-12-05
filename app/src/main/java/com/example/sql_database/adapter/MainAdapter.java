397a13a633f10548e753f07158aff21cee0735af
MadMakZa <immakza@gmail.com>
2020-12-05 15:48:15 +0300

    class MyViewHolder for searching

--- app/src/main/java/com/example/sql_database/adapter/MainAdapter.java
+++ app/src/main/java/com/example/sql_database/adapter/MainAdapter.java
@@ -1,15 +1,22 @@
 package com.example.sql_database.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sql_database.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Адаптер recycler view
+ */
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> {
     //принимает контекст с вью
     private Context context;
@@ -20,26 +27,36 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> 
         mainArray = new ArrayList<>();
     }
 
+    //метод который рисует на экране разметку
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //создаем вью и передаем новый лайоут для разметки
+        View view = LayoutInflater.from(context).inflate(R.layout.item_list_layout, parent, false);
+        return new MyViewHolder(view);
     }
-
+    //заполняет элементами
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
     }
-
+    //отвечает за количество запусков методов которые выше
     @Override
     public int getItemCount() {
         return mainArray.size();
     }
 
+    //вложенный класс для нахождения элемента который будет рисоваться
     class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvTitle;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+        }
+        //берем с List наш текст
+        public void setData(String title){
+            tvTitle.setText(title);
         }
     }
     public void updateAdapter(List<String> newList){
