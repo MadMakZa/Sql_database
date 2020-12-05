76b5fe62581d3a2c5c3a0bfe45f5c108046c0a10
MadMakZa <immakza@gmail.com>
2020-12-05 20:54:15 +0300

    add listener for positions

--- app/src/main/java/com/example/sql_database/adapter/MainAdapter.java
+++ app/src/main/java/com/example/sql_database/adapter/MainAdapter.java
@@ -1,6 +1,7 @@
 package com.example.sql_database.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +21,7 @@ import java.util.List;
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> {
     //принимает контекст с вью
     private Context context;
-    private List<String> mainArray;
+    private List<ListItem> mainArray;
     //конструктор который создается вместе с recycler view
     public MainAdapter(Context context) {
         this.context = context;
@@ -38,7 +39,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> 
     //заполняет элементами
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.setData(mainArray.get(position));
+        holder.setData(mainArray.get(position).getTitle());
     }
     //отвечает за количество запусков методов которые выше
     @Override
@@ -47,19 +48,25 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> 
     }
 
     //вложенный класс для нахождения элемента который будет отрисован
-    class MyViewHolder extends RecyclerView.ViewHolder {
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView tvTitle;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
+            itemView.setOnClickListener(this);
         }
         //берем с List наш текст
         public void setData(String title){
             tvTitle.setText(title);
         }
+        //слушатель нажатий каждого добавленного элемента в ресайклер вью
+        @Override
+        public void onClick(View v) {
+            Log.d("Mylog", "Pressed : " + getAdapterPosition());
+        }
     }
-    public void updateAdapter(List<String> newList){
+    public void updateAdapter(List<ListItem> newList){
         mainArray.clear();
         mainArray.addAll(newList);
         notifyDataSetChanged(); //передать в адаптер
